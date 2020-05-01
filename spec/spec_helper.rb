524bdc3e32dbc418aa9945b73ec31648feba7405
Rik Vanmechelen <rik_vanmechelen@moma.org>
2020-04-30 23:35:30 -0400

    updated gems. cmdline tests still failing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,15 @@ require 'bundler'
 require 'json'
 require 'simplecov'
 require 'grocer'
+require 'rspec/its'
 SimpleCov.start
 
 Dir['lib/passbook/**/*.rb'].each {|f| require File.join(File.dirname(__FILE__), '..', f.gsub(/.rb/, ''))}
 Dir['lib/rack/**/*.rb'].each {|f| require File.join(File.dirname(__FILE__), '..', f.gsub(/.rb/, ''))}
+
+
+RSpec.configure do |config|
+    config.expect_with :rspec do |expectations|
+        expectations.syntax = [:expect, :should]
+    end
+end
